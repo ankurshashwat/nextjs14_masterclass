2c2c8ddbf932cb748625673c05c6cf21ca311be2
ankurshashwat <ankurshashwt@gmail.com>
2024-02-07 21:36:31 +0530

    fixed global search ui

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 const GlobalSearch = () => {
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
-      <div className="relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+      <div className="relative flex min-h-[56px] background-light800_darkgradient grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
           alt="search"
@@ -16,8 +16,8 @@ const GlobalSearch = () => {
         <Input
           type="text"
           placeholder="Search..."
-          value=""
-          className="parahgraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          value=" "
+          className="parahgraph-regular no-focus placeholder bg-transparent border-none shadow-none outline-none"
         />
       </div>
     </div>
